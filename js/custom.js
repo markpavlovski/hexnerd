d69cee512c2444d4ba24f3fd3fa7bf0944e4c7b6
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-08 11:53:18 -0800

    added basic grid layout

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,1 @@
 console.log("Loaded custom JS")
-$( "#window-2" ).click(function (){
-	$( ".editor" ).addClass( "editor--2-windows-layout" );
-  $( "#window-1" ).show();
-  $( "#window-2" ).hide();
-});
-
-$( "#window-1" ).click(function (){
-	$( ".editor" ).removeClass( "editor--2-windows-layout" );
-  $( "#window-2" ).show();
-  $( "#window-1" ).hide();
-});
