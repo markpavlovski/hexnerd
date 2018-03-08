3bf0f2cab845ad93ca053f843196bdddc715425f
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-08 11:47:28 -0800

    added sass example that looks like atom. scrapping this and restarting going forward

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,12 @@
 console.log("Loaded custom JS")
+$( "#window-2" ).click(function (){
+	$( ".editor" ).addClass( "editor--2-windows-layout" );
+  $( "#window-1" ).show();
+  $( "#window-2" ).hide();
+});
+
+$( "#window-1" ).click(function (){
+	$( ".editor" ).removeClass( "editor--2-windows-layout" );
+  $( "#window-2" ).show();
+  $( "#window-1" ).hide();
+});
