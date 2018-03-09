0607651d05e0ffca6ae2c80c2ffa40716dc02db3
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-08 23:08:29 -0800

    added smooth transitions, fixed extra width scrolling bug, added a state object to keep a track of everything, added full screen mode

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,50 @@
 console.log("Loaded custom JS")
 
-function showAside() {
+const state = {
+  nav: true,
+  fade: false,
+  aside: false,
+  full: false
+}
 
+// Side bar transitions
+function showAside() {
   const aside = document.querySelector("aside")
   const fade = document.querySelector(".fade")
 
-  // Initial fade is set to opacity: 0; z-index: -1
   fade.classList.add("z4")
-  window.setTimeout(() => {
-    fade.classList.add("show")
-  }, 5)
+  window.setTimeout(() => fade.classList.add("show"), 5)
+  state.fade = true
 
-  // Initial aside is set to margin 100%, z-index -1
   aside.classList.add("z5")
-  window.setTimeout(() => {
-    aside.classList.add("show")
-  }, 5)
-
+  window.setTimeout(() => aside.classList.add("show"), 5)
+  state.aside = true
 }
 
 function hideAside() {
   const aside = document.querySelector("aside")
   const fade = document.querySelector(".fade")
 
-  // Initial aside is set to margin 100%, z-index -1
-  aside.classList.remove("show")
-	window.setTimeout(()=>aside.classList.remove("z5"),500)
-
-  // Initial fade is set to opacity: 0; z-index: -1
   fade.classList.remove("show")
-	window.setTimeout(()=>fade.classList.remove("z4"),500)
+  window.setTimeout(() => fade.classList.remove("z4"), 500)
+  state.fade = false
 
+  aside.classList.remove("show")
+  window.setTimeout(() => aside.classList.remove("z5"), 500)
+  state.aside = false
+}
+
+// Full screen mode
+function showFull() {
+  const fullScreen = document.querySelector(".full-screen")
+  fullScreen.classList.add("z6")
+  window.setTimeout(() => fullScreen.classList.add("show"), 5)
+	state.full = true
+}
+
+function hideFull() {
+  const fullScreen = document.querySelector(".full-screen")
+  fullScreen.classList.remove("show")
+  window.setTimeout(() => fullScreen.classList.remove("z6"), 500)
+	state.full = false
 }
