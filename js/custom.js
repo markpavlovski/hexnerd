ce4d3a97e008605c8d95c9cce340b772438e3b9b
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-08 17:46:26 -0800

    working on the grey out transition

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 console.log("Loaded custom JS")
 window.setTimeout(()=>{
 	console.log(document.querySelector('aside'))
+	console.log(document.querySelector('.grey-fade'))
 	document.querySelector('aside').style = 'opacity: 1'
+	document.querySelector('.grey-fade').style = 'opacity: 0'
 },1200)
+
+
+// Trigger for the right side bar:
+// document.querySelector("aside").classList.toggle("unhidden")
