a5d7e4f05fabf345a753b3ebdaf0fc8ee6f0d674
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-09 13:30:04 -0800

    added mobile swipe detection

--- js/custom.js
+++ js/custom.js
@@ -86,3 +86,53 @@ document.addEventListener('keydown', (event) => {
   if (keyName === 'Escape' || keyName.toLowerCase() === 'q') hideFull()
 
 });
+
+
+// detect mobile
+//
+//
+// function isMobileDevice() {
+//     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+// };
+//
+// alert(isMobileDevice())
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+        } else {
+            /* right swipe */
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            hideFull()
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
