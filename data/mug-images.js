df4fbfa55cc84a7a566e88c1c0ca8ff52fea0efd
Mark Pavlovski <markpavlovski@gmail.com>
2018-03-13 21:06:19 -0700

    added image template and coffee design

--- data/mug-images.js
+++ data/mug-images.js
@@ -1,4 +1,5 @@
 const mugImages = {
-  "#c0ffee": "https://ctl.s6img.com/society6/img/Jb07fMNxyXZdjuDUuFlv_oVyaVo/w_700/coffee-mugs/small/right/greybg/~artwork,fw_4600,fh_2000,iw_4600,ih_2000/s6-original-art-uploads/society6/uploads/misc/fcb52e79d62b4c688c80e8b3dc6a3644/~~/coffee1155242-mugs.jpg",
+  "#c0ffee": "https://ctl.s6img.com/society6/img/lMBMXtNsf5tJte0GN70Q1BhcRG0/w_700/coffee-mugs/small/right/greybg/~artwork,fw_4600,fh_2000,iw_4600,ih_2000/s6-original-art-uploads/society6/uploads/misc/c9ccdde1cf9a4240984e737df6363d8c/~~/coffee-hex-rgb-mugs.jpg",
 }
+
 console.log("mug images loaded")
